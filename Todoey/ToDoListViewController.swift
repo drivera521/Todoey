22f93c4465161238ecc3ade3b6556693fa7eed34
Daniel Rivera <drivera521@gmail.com>
2020-09-25 14:22:36 -0400

    Todo list complete with Datasource method.

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Daniel Rivera on 9/25/20.
+//  Copyright © 2020 Daniel Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController:  UITableViewController {
+
+    let itemList = ["Buy Eggos", "Find Mike", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return itemList.count
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemList[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - Tableview Delegate Method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        if cell?.accessoryType == .checkmark{
+            cell?.accessoryType = .none
+        } else {
+            cell?.accessoryType = .checkmark
+        }
+    
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
